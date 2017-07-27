1e08e754e4cf21063b826611c2a0cc27a45ff491
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-07-26 22:00:11 -0300

    Add securekeys to random repo

--- app/src/main/java/com/sqisland/android/test_demo/DemoApplication.java
+++ app/src/main/java/com/sqisland/android/test_demo/DemoApplication.java
@@ -2,22 +2,43 @@ package com.sqisland.android.test_demo;
 
 import android.app.Application;
 
+import com.u.securekeys.SecureEnvironment;
+import com.u.securekeys.annotation.SecureKey;
 import javax.inject.Singleton;
 
 import dagger.Component;
 
+@SecureKey(
+    key = "One",
+    value = "Uno"
+)
 public class DemoApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SecureEnvironment.initialize(this);
+  }
+
   @Singleton
   @Component(modules = ClockModule.class)
   public interface ApplicationComponent extends DemoComponent {
   }
 
+  @SecureKey(
+      key = "Two",
+      value = "Dos"
+  )
   private final DemoComponent component = createComponent();
 
   protected DemoComponent createComponent() {
     return DaggerDemoComponent.builder().build();
   }
 
+  @SecureKey(
+      key = "Three",
+      value = "Tres"
+  )
   public DemoComponent component() {
     return component;
   }
--- app/src/main/java/com/sqisland/android/test_demo/MainActivity.java
+++ app/src/main/java/com/sqisland/android/test_demo/MainActivity.java
@@ -4,10 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.u.securekeys.SecureEnvironment;
+import com.u.securekeys.annotation.SecureConfigurations;
+import com.u.securekeys.annotation.SecureKey;
+import com.u.securekeys.annotation.SecureKeys;
+import junit.framework.Assert;
 import org.joda.time.DateTime;
 
 import javax.inject.Inject;
 
+@SecureKey(key = "Activity", value = "Value")
+@SecureConfigurations(
+    useAesRandomly = true
+)
 public class MainActivity extends Activity {
   public static final String KEY_MILLIS = "millis";
 
@@ -26,5 +35,11 @@ public class MainActivity extends Activity {
     long millis = getIntent().getLongExtra(KEY_MILLIS, -1);
     DateTime dateTime = (millis > 0) ? new DateTime(millis) : clock.getNow();
     todayView.setText(DateUtils.format(dateTime));
+
+    Assert.assertFalse(SecureEnvironment.getString("Activity").isEmpty());
+    Assert.assertFalse(SecureEnvironment.getString("One").isEmpty());
+    Assert.assertFalse(SecureEnvironment.getString("Two").isEmpty());
+    Assert.assertFalse(SecureEnvironment.getString("Three").isEmpty());
+
   }
 }
